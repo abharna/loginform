407a4fb699ddc3294cf0ef9e40732eb07968496c
abharna <35066612+abharna@users.noreply.github.com>
2019-08-13 14:21:48 +0530

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Modules from './components/testing' 
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+          <Modules/>
+        </div>
+    )
+  }
+}
+
+
+
+export default App;
